5116fd1bb95c584163ad0b91fcceba23e3483922
Fitiavana_R <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2024-12-04 09:28:09 +0200

    re-init

--- BackEnd Nodejs/index.js
+++ BackEnd Nodejs/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const uploadRoutes = require('./routes/csvRoutes');
+const uploadRoutes = require('./api/routes/csvRoutes');
 const cors = require('cors');
 
 const app = express();
