e9537678d1feaaa543c080d31965513267d76eb4
Fitiavana_R <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2024-12-04 22:49:34 +0200

    re-init

--- FrontEnd Angular/src/app/services/csv/file-upload.service.ts
+++ FrontEnd Angular/src/app/services/csv/file-upload.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CsvUploadService {
 
-  private apiUrl = 'http://localhost:3000/api';  // Remplacez par l'URL de votre backend
+
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ FrontEnd Angular/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://back-end-nodejs-a2tvzezou-fitiavanarazakarivonys-projects.vercel.app/api'
+};
