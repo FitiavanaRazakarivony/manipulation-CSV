e8537715bc22716a1207135dc749db7af48287bc
Fitiavana_R <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2024-12-09 08:42:39 +0200

    re

--- backend/api/middlewares/FileUploadMiddleware.js
+++ backend/api/middlewares/FileUploadMiddleware.js
@@ -1,48 +1,47 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
 const { validateFileType } = require('../utils/FileUtils');
 const config = require('../../config/appConfig');
-const { cleanData } = require('../utils/JoinUtils');  // Importation de cleanData
+const { cleanData } = require('../utils/JoinUtils');
 const fastcsv = require('fast-csv');
+const fs = require('fs');
+
+// Vous pouvez supprimer l'utilisation de "fs" car les fichiers persistants ne fonctionneront pas sur Vercel.
 
 class FileUploadMiddleware {
-
-  constructor(uploadDir = config.uploadDir) {
-    this.uploadDir = path.join(__dirname, '../../', uploadDir);  // Dossier où les fichiers seront stockés
-
-    this.ensureUploadDirExists();
-
+  constructor() {
     this.upload = multer({
-      storage: this.storage(),
-      fileFilter: this.fileFilter,  // Garder la vérification de type de fichier
+      storage: this.diskStorage(), // Utilisation de la mémoire pour stocker temporairement les fichiers
+      fileFilter: this.fileFilter,
     });
   }
 
-  ensureUploadDirExists() {
-    if (!fs.existsSync(this.uploadDir)) {
-      fs.mkdirSync(this.uploadDir, { recursive: true });  // Crée le répertoire de téléchargement s'il n'existe pas
-    }
-  }
-
-  storage() {
+  diskStorage() {
     return multer.diskStorage({
-      destination: (req, file, cb) => cb(null, this.uploadDir),
-      filename: (req, file, cb) => {
-        const customName = `${req.body.uploadFile || 'default'}-${new Date().toLocaleDateString('fr-FR').replace(/\//g, '-')}-${file.originalname}`;
-        cb(null, customName);
+      destination: (req, file, cb) => {
+        const uploadDir = './uploads';
+  
+        // Vérifier si le dossier 'uploads' existe, sinon le créer
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir, { recursive: true }); // Créer le dossier si nécessaire
+        }
+  
+        // Définir le dossier de destination
+        cb(null, uploadDir);
       },
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+      }
     });
   }
-
   fileFilter(req, file, cb) {
-    validateFileType(file, cb);  // Validation du type de fichier
+    validateFileType(file, cb); // Validation du type de fichier
   }
 
   getMiddleware() {
     return this.upload;
   }
-
 }
 
 module.exports = FileUploadMiddleware;
